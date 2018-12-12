f7ef660d9c897a8da307fb6fca428eb983767b45
Amit Bhattacharjee <amit@orastays.com>
2018-12-12 19:19:59 +0530

    Uploaded changes file in home page, footer, header responsive

--- src/app/modules/design/design-routing.module.ts
+++ src/app/modules/design/design-routing.module.ts
@@ -3,12 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { PropertyListComponent } from './property-list/property-list.component';
 import { HostListingComponent } from './host-listing/host-listing.component';
+import { PropertyDetailsComponent} from './property-details/property-details.component';
 
 
 const routerConfig: Routes = [
   { path: '', component: HomeComponent},
   { path: 'property-list', component: PropertyListComponent},
   { path: 'host-listing', component: HostListingComponent},
+  { path: 'property-details', component: PropertyDetailsComponent},
 ];
 
 @NgModule({
--- src/app/modules/design/design.module.ts
+++ src/app/modules/design/design.module.ts
@@ -3,21 +3,26 @@ import { CommonModule } from '@angular/common';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { UiSwitchModule } from 'ngx-ui-switch';
+import { NgxGalleryModule } from 'ngx-gallery';
+import { DragScrollModule } from 'ngx-drag-scroll';
 
 import { DesignRoutingModule } from './design-routing.module';
 import { HomeComponent } from './home/home.component';
 import { PropertyListComponent } from './property-list/property-list.component';
 import { HostListingComponent } from './host-listing/host-listing.component';
+import { PropertyDetailsComponent } from './property-details/property-details.component';
 
 @NgModule({
-  declarations: [HomeComponent, PropertyListComponent, HostListingComponent],
+  declarations: [HomeComponent, PropertyListComponent, HostListingComponent, PropertyDetailsComponent],
   imports: [
     CommonModule,
     NgbModule,
     FormsModule,
     ReactiveFormsModule,
     UiSwitchModule,
-    DesignRoutingModule
+    DesignRoutingModule,
+    NgxGalleryModule,
+    DragScrollModule
   ]
 })
 export class DesignModule { }
