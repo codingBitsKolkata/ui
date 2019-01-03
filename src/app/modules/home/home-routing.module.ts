6e39903d8546dac0139c09d64c762a02b1043988
Arvind <arvindk427@gmail.com>
2019-01-03 23:58:37 +0530

    Testimonial page

--- src/app/modules/home/home-routing.module.ts
+++ src/app/modules/home/home-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home.component';
+import { TestimonialComponent } from './testimonial/testimonial.component';
 
 const routerConfig: Routes = [
   { path: '', component: HomeComponent},
+  { path: 'testimonial', component: TestimonialComponent},
 ];
 
 @NgModule({
