7040ce5debea9bd36301be0aac42389f3e49daf4
Amit Bhattacharjee <amit@orastays.com>
2019-01-04 19:52:47 +0530

    Host add form

--- src/app/modules/design/host-listing/host-listing.component.ts
+++ src/app/modules/design/host-listing/host-listing.component.ts
@@ -6,22 +6,72 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./host-listing.component.scss']
 })
 export class HostListingComponent implements OnInit {
+  
+  specialExperienceList = [];
+  specialExperience = [];
+
+  specialitiesList = [];
+  specialities = [];
+
+  amenitiesList = [];
+  amenities = [];
+
+  dropdownSettings = {};
 
   constructor() { }
 
   ngOnInit() {
+    this.specialExperienceList = [
+        { item_id: 1, item_text: 'Exp 1' },
+        { item_id: 2, item_text: 'Exp 2' },
+        { item_id: 3, item_text: 'Exp 3' },
+        { item_id: 4, item_text: 'Exp 4' },
+        { item_id: 5, item_text: 'Exp 5' },
+        { item_id: 6, item_text: 'Exp 6' },
+        { item_id: 7, item_text: 'Exp 7' },
+        { item_id: 8, item_text: 'Exp 8' }
+    ];
+    this.specialExperience = [];
+    
+    this.specialitiesList = [
+      { item_id: 1, item_text: 'Specialities 1' },
+      { item_id: 2, item_text: 'Specialities 2' },
+      { item_id: 3, item_text: 'Specialities 3' },
+      { item_id: 4, item_text: 'Specialities 4' },
+      { item_id: 5, item_text: 'Specialities 5' },
+      { item_id: 6, item_text: 'Specialities 6' },
+      { item_id: 7, item_text: 'Specialities 7' },
+      { item_id: 8, item_text: 'Specialities 8' }
+    ];
+    this.specialExperience = [];
+    
+    this.amenitiesList = [
+      { item_id: 1, item_text: 'Amenities 1' },
+      { item_id: 2, item_text: 'Amenities 2' },
+      { item_id: 3, item_text: 'Amenities 3' },
+      { item_id: 4, item_text: 'Amenities 4' },
+      { item_id: 5, item_text: 'Amenities 5' },
+      { item_id: 6, item_text: 'Amenities 6' },
+      { item_id: 7, item_text: 'Amenities 7' },
+      { item_id: 8, item_text: 'Amenities 8' }
+    ];
+    this.specialities = [];
+
+    this.dropdownSettings = {
+        singleSelection: false,
+        idField: 'item_id',
+        textField: 'item_text',
+        selectAllText: 'Select All',
+        unSelectAllText: 'UnSelect All',
+        itemsShowLimit: 3,
+        allowSearchFilter: true
+    };
   }
+
   scroll(el) {
     el.scrollIntoView({behavior: 'smooth'});
   }
 
-  // Host Form parameters
-  guestAccess = [];
-  nearbyPlaces = [];
-  spaceRules = ['Couple Friendly', 'Pet Friendly', 'Smoking', 'Alchohol'];
-  specialExperience = ['Spa', 'Musical Instrument', 'Yoga Center'];
-  specialities = ['Item 1', 'Item 2', 'Item 3', 'Item 4', 'Item 5'];
-  amenities = ['Item 1', 'Item 2', 'Item 3', 'Item 4', 'Item 5'];
   checkInTime = {hour: 10, minute: 30};
   checkOutTime = {hour: 12, minute: 30};
   meridian = true;
