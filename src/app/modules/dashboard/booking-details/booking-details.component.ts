1ba67502f9c63d54e9bae2fe9d1a17239c8c5609
Amit Bhattacharjee <amit@orastays.com>
2019-01-07 11:34:08 +0530

    Commiting all changes

--- src/app/modules/dashboard/booking-details/booking-details.component.ts
+++ src/app/modules/dashboard/booking-details/booking-details.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-booking-details',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BookingDetailsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private modalService: NgbModal) { }
 
   ngOnInit() {
+  }
+  openModal(content) {
+    // , size: 'md'
+    this.modalService.open(content, { windowClass: 'modal-popup cancellation-modal', size:'sm', centered: true });
   }
 
 }
