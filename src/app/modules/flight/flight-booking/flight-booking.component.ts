c3e012c46896bcf1866cc6607d3711b9f7551792
Amit Bhattacharjee <amit@orastays.com>
2019-01-11 17:12:01 +0530

    Flight booking select meal, select baggege and select seats

--- src/app/modules/flight/flight-booking/flight-booking.component.ts
+++ src/app/modules/flight/flight-booking/flight-booking.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 @Component({
   selector: 'app-flight-booking',
   templateUrl: './flight-booking.component.html',
@@ -8,9 +8,23 @@ import { Component, OnInit } from '@angular/core';
 export class FlightBookingComponent implements OnInit {
   panelOpenState = false;
   step = 1;
-  constructor() { }
+  totalSeatCol;
+  constructor(private modalService: NgbModal) { }
 
   ngOnInit() {
+    this.totalSeatCol = 30;
   }
 
+  openModal(content) {
+    // , size: 'md'
+      this.modalService.open(content, { windowClass: 'modal-popup flight-service-modal', centered: true, size: 'lg' });
+  }
+
+  seatsArray(number){
+    var items: number[] = [];
+    for(var i = 1; i <= number; i++){
+       items.push(i);
+    }
+    return items;
+  }
 }
