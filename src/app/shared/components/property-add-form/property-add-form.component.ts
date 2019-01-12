dbd20b33861e9a2fbce878284ae58307b8c884c0
Rahul Ghosh <rahul.ghosh33@gmail.com>
2019-01-12 11:43:07 +0530

    add property object

--- src/app/shared/components/property-add-form/property-add-form.component.ts
+++ src/app/shared/components/property-add-form/property-add-form.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
+import { PropertyService } from '../../../services/apis/property.service';
 
 @Component({
   selector: 'app-property-add-form',
@@ -22,7 +23,8 @@ export class PropertyAddFormComponent implements OnInit {
   totalRoomsVal: any;
 
   constructor(
-    private fb: FormBuilder
+    private fb: FormBuilder,
+    private srvProperty: PropertyService,
   ) { 
     this.buildAddPropertyForm();
     this.totalRoomsVal = 1;
@@ -186,8 +188,312 @@ export class PropertyAddFormComponent implements OnInit {
     console.log('Property Form =>', this.addPropertyForm);
   }
 
+  specialExperienceArr () {
+    let arr = [];
+    this.addPropertyForm.get('step4')['controls'].specialExperience.value.forEach(exp => {
+       arr.push({
+        "specialExperience": { 
+          "experienceId": exp.item_id,
+          "experienceName": exp.item_text
+        }
+      })
+    })
+    return arr;
+  }
+
   submitPropertyForm() {
     console.log('Form =>', this.addPropertyForm);
+    // const arr: Array<object> = this.specialExperienceArr();
+    console.log('Arr =>',  this.specialExperienceArr());
+    
+    const addPropertyRequest = {
+      "name": this.addPropertyForm.get('step2')['controls'].name.value,
+      "address": this.addPropertyForm.get('step2')['controls'].address.value,
+      "latitude": "22.582490",
+      "longitude": "88.426787",
+      "entireApartment": this.addPropertyForm.get('step7')['controls'].entireApartment.value,
+      "apartmentName": this.addPropertyForm.get('step8')['controls'].apartmentName.value,
+      "apartmentNumber": this.addPropertyForm.get('step8')['controls'].apartmentNumber.value,
+      "startDate": this.addPropertyForm.get('step23')['controls'].startDate.value,
+      "endDate": this.addPropertyForm.get('step23')['controls'].endDate.value,
+      "checkinTime": this.addPropertyForm.get('step5')['controls'].checkinTime.value,
+      "checkoutTime": this.addPropertyForm.get('step5')['controls'].checkoutTime.value,
+      "userToken": "89ef239b-973f-4655-b691-4064e61910f9",
+      "strictCheckin": this.addPropertyForm.get('step5')['controls'].strictCheckin.value,
+      "coverImageUrl": this.addPropertyForm.get('step6')['controls'].coverPhotoImg.value,
+      "priceDrop": this.addPropertyForm.get('step19')['controls'].priceDrop.value,
+      "immediateBooking": "Y",
+      "languageId": "1",
+      "contactName": this.addPropertyForm.get('step24')['controls'].contactName.value,
+      "altMobile": this.addPropertyForm.get('step24')['controls'].altMobile.value,
+      "landline": this.addPropertyForm.get('step24')['controls'].landline.value,
+      "altEmail": this.addPropertyForm.get('step24')['controls'].altEmail.value,
+      "advancePercentage": "100",
+      "sexCategory": this.addPropertyForm.get('step10')['controls'].sexCategory.value,
+      "propertyType": {
+        "name": this.addPropertyForm.get('step1')['controls'].propertyType.value,
+        "propertyTypeId": "1"
+      },
+      "propertyVsDescriptions": [
+        {
+          "description": this.addPropertyForm.get('step1')['controls'].description.value,
+          "languageId": "1"
+        }
+      ],   "propertyVsPriceDrops": [
+          {
+            "status": 1,
+            "createdDate": "2018-12-29 22:03:23",
+            "createdBy": 1,
+            "modifiedDate": null,
+            "modifiedBy": null,
+            "userToken": null,
+            "languageId": null,
+            "propertyPDropId": "59",
+            "percentage": "0",
+            "property": null,
+            "priceDrop": {
+              "status": 1,
+              "createdDate": "2018-09-06 01:27:34",
+              "createdBy": 1,
+              "modifiedDate": null,
+              "modifiedBy": null,
+              "userToken": null,
+              "languageId": null,
+              "priceDropId": "1",
+              "afterTime": "8",
+              "propertyVsPriceDrops": null
+            }
+          },
+          {
+            "status": 1,
+            "createdDate": "2018-12-29 22:03:23",
+            "createdBy": 1,
+            "modifiedDate": null,
+            "modifiedBy": null,
+            "userToken": null,
+            "languageId": null,
+            "propertyPDropId": "60",
+            "percentage": "0",
+            "property": null,
+            "priceDrop": {
+              "status": 1,
+              "createdDate": "2018-09-06 01:27:34",
+              "createdBy": 1,
+              "modifiedDate": null,
+              "modifiedBy": null,
+              "userToken": null,
+              "languageId": null,
+              "priceDropId": "2",
+              "afterTime": "11",
+              "propertyVsPriceDrops": null
+            }
+          }
+        ],
+      "propertyVsDocuments": [
+        {
+          "documentNumber": "AKBHP6784S",
+          "fileUrl": "https://s-ec.bstatic.com/images/hotel/max1024x768/141/141830278.jpg",
+          "document": {
+            "documentId": "1",
+            "documentName": "PAN CARD"
+          }
+        },
+        {
+          "documentNumber": "",
+          "fileUrl": "https://s-ec.bstatic.com/images/hotel/max1024x768/141/141830278.jpg",
+          "document": {
+            "documentId": "2",
+            "documentName": "CANCELLED CHEQUE"
+          }
+        }
+      ],
+      "propertyVsGuestAccess": [
+        {
+          "guestAccess": "Guest Can Access Terrace",
+          "languageId": "1"
+        },
+        {
+          "guestAccess": "Access To Lawn",
+          "languageId": "1"
+        }
+      ],
+      "propertyVsImages": [
+        {
+          "imageURL": "https://s-ec.bstatic.com/images/hotel/max1024x768/141/141830278.jpg"
+        },
+        {
+          "imageURL": "https://s-ec.bstatic.com/images/hotel/max1024x768/141/141830278.jpg"
+        },
+        {
+          "imageURL": "https://s-ec.bstatic.com/images/hotel/max1024x768/141/141830278.jpg"
+        }
+      ],
+      "propertyVsNearbys": [
+        {
+          "places": "RS Software"
+        },
+        {
+          "places": "SDF"
+        },
+        {
+          "places": "Nicco Park"
+        }
+      ],
+      "propertyVsSpaceRules": [
+        {
+          "answer": "Y",
+          "spaceRule": {
+            "spruleId": "1",
+            "ruleName": "Smoking"
+          }
+        },
+        {
+          "answer": "N",
+          "spaceRule": {
+            "spruleId": "2",
+            "ruleName": "Drinking"
+          }
+        }
+      ],
+      "propertyVsSpecialExperiences": this.specialExperienceArr(),
+      "rooms": [
+        { 
+          "cotAvailable": "Y",
+          "noOfChild": "1",
+          "noOfGuest": "2",
+          "numOfCot": "1",
+          "sharedSpace": "N",
+          "accommodationName": "PRIVATE",
+          "noOfBeds": "",
+          "roomPricePerNight": "1500",
+          "roomPricePerMonth": "",
+          "sharedBedPricePerNight": "",
+          "sharedBedPricePerMonth": "",
+          "cotPrice": "200",
+          "sharedBedPrice": "",
+          "commission": "5",
+          "hostDiscountWeekly": "2",
+          "hostDiscountMonthly": "",
+          "roomCategory": {
+            "roomCatId": "1",
+            "name": "Delux"
+          },
+              
+               "roomVsCancellations": [
+            {
+              "percentage": "50",
+              "cancellationSlab": {
+                "cancellationSlabId": "1"
+              }
+            },
+            {
+              "percentage": "40",
+              "cancellationSlab": {
+                "cancellationSlabId": "2"
+              }
+            }
+          ],
+          "roomVsAmenities": [
+            {
+              "amenities": {
+                "aminitiesId": "1",
+                "aminitiesName": "WIFI"
+              }
+            }
+          ],
+          "roomVsCancellation": [
+            {
+              "percentage": "50",
+              "cancellationSlab": {
+                "cancellationSlabId": "1"
+              }
+            },
+            {
+              "percentage": "40",
+              "cancellationSlab": {
+                "cancellationSlabId": "2"
+              }
+            }
+          ],
+          "roomVsImages": [
+            {
+              "imageURL": "https://s-ec.bstatic.com/images/hotel/max1024x768/141/141830278.jpg"
+            },
+            {
+              "imageURL": "https://s-ec.bstatic.com/images/hotel/max1024x768/141/141830278.jpg"
+            },
+            {
+              "imageURL": "https://s-ec.bstatic.com/images/hotel/max1024x768/141/141830278.jpg"
+            }
+          ],
+          "roomVsMeals": [
+            {
+              "mealPlan": {
+                "mealPlanId": "1",
+                "mealPlanName": "Break Fast"
+              },
+              "mealDaysSunday": "Y",
+              "mealDaysMonday": "Y",
+              "mealDaysTuesday": "Y",
+              "mealDaysWednesday": "Y",
+              "mealDaysThursday": "Y",
+              "mealDaysFriday": "Y",
+              "mealDaysSaturday": "Y",
+                      "mealTypeSunday": "BOTH",
+              "mealTypeMonday": "BOTH",
+              "mealTypeTuesday": "BOTH",
+              "mealTypeWednesday": "BOTH",
+              "mealTypeThursday": "BOTH",
+              "mealTypeFriday": "BOTH",
+              "mealTypeSaturday": "BOTH",
+              "mealPriceCategorySunday": "COMPLIMENTARY",
+              "mealPriceCategoryMonday": "COMPLIMENTARY",
+              "mealPriceCategoryTuesday": "COMPLIMENTARY",
+              "mealPriceCategoryWednesday": "COMPLIMENTARY",
+              "mealPriceCategoryThursday": "COMPLIMENTARY",
+              "mealPriceCategoryFriday": "COMPLIMENTARY",
+              "mealPriceCategorySaturday": "COMPLIMENTARY"
+              
+            }
+          ],
+          "roomVsSpecialities": [
+            {
+              "specialties": {
+                "specialitiesName": "Mountain Facing",
+                "specialtiesId": "1"
+              }
+            },
+            {
+              "specialties": {
+                "specialitiesName": "Sea Facing",
+                "specialtiesId": "2"
+              }
+            }
+          ]
+        }
+      ],
+      "stayType": {
+        "stayTypeId": "2",
+        "stayTypeName": "Short Term"
+      },
+      "hostVsAccount": {
+        "accountHolderName": "Sudeep Chhetri",
+        "accountNumber": "159883741610",
+        "accountType": "Saving",
+        "bankName": "IndusInd Bank",
+        "branchName": "Sector V",
+        "ifscCode": "IND0015478"
+      }
+    }
+        
+    this.srvProperty.addProperty(addPropertyRequest).subscribe((res) => {
+      console.log('Propert Add Dadadata', res);
+      if (res.responseCode === '200') {
+         
+      }
+    }, error => {
+      console.log('error', error);
+    });
   }
 
   onCoverImgChange($event) {
